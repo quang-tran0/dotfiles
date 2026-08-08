cc4f73e1a7dd9879b7e42b635decc5579d9c29bf
Quang Tran <quang.trd05@gmail.com>
2026-08-08 23:23:27 +0700

    Update nvim plugins

--- home/dot_config/nvim/lua/plugins/oil.lua
+++ home/dot_config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,10 @@ return {
     {
         "stevearc/oil.nvim",
 
+        keys = {
+            { "<leader>cd", "<cmd>Oil<cr>", desc = "Open parent directory" },
+        },
+
         opts = {
             keymaps = {
                 ["<Right>"] = "actions.select",
