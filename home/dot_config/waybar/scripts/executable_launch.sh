4d9b47e3b6536e7f9fd70336fbcb1eae10bfcb35
Quang Tran <quang.trd05@gmail.com>
2026-08-11 13:19:50 +0700

    Update dotfiles

--- home/dot_config/waybar/scripts/executable_launch.sh
+++ home/dot_config/waybar/scripts/executable_launch.sh
@@ -2,4 +2,4 @@
 
 pkill -x waybar 2>/dev/null
 exec waybar --config /home/quang-tran/.config/waybar/config.jsonc \
-  --style /home/quang-tran/.config/waybar/style.css
+  --style /home/quang-tran/.config/waybar/style.css &
