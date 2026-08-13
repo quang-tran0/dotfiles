b2f964f692049945f47472fa4b1532e2579b4304
Quang Tran <quang.trd05@gmail.com>
2026-08-13 17:25:23 +0700

    Update dotfiles

--- home/dot_config/hypr/config/look/general.lua
+++ home/dot_config/hypr/config/look/general.lua
@@ -7,16 +7,16 @@ local c = require("config.look.colors")
 
 hl.config({
     general = {
-        gaps_in  = 5,
-        gaps_out = 10,
+        gaps_in          = 5,
+        gaps_out         = 10,
 
-        border_size = 2,
+        border_size      = 2,
 
-        col = {
+        col              = {
             -- Catppuccin Frappe: lavender -> mauve -> blue gradient border.
-            active_border        = { colors = { c.lavender, c.mauve, c.blue }, angle = 135 },
-            inactive_border      = c.overlay1_dim,
-            nogroup_border       = c.yellow_dim,
+            active_border         = { colors = { c.lavender, c.mauve, c.blue }, angle = 135 },
+            inactive_border       = c.overlay1_dim,
+            nogroup_border        = c.yellow_dim,
             nogroup_border_active = c.peach,
         },
 
@@ -24,8 +24,11 @@ hl.config({
         resize_on_border = false,
 
         -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-        allow_tearing = false,
+        allow_tearing    = false,
 
         -- layout = "dwindle",
+    },
+    xwayland = {
+        force_zero_scaling = true,
     },
 })
