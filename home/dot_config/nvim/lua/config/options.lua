48f13d7f30d916958c90cb7adb254f2a4cc571e4
Quang Tran <quang.trd05@gmail.com>
2026-09-06 21:16:04 +0700

    Update dotfiles

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -8,3 +8,9 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.whichwrap:append("<,>,[,],h,l")
+
+vim.opt.clipboard = "unnamedplus"
+
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+    vim.g.clipboard = "osc52"
+end
